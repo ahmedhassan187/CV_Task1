591ba28feab1c1bcd7c97185a01c061dbc973c77
Mohamed Nasser <nassermohamed3222@gmail.com>
2023-03-06 22:12:20 +0200

    local theshold

--- thresholding.py
+++ thresholding.py
@@ -33,14 +33,50 @@ def global_threshold(image, thres_value, val_high, val_low):
     return img
 
 
+def local_threshold(image, size):
+    img = image.copy()
+    i = 0
+    j = 0
+    while i < img.shape[0]:
+        j = 0
+        while j < img.shape[1]:
+            x = i
+            mean = 0
+            cnt = 0
+            while x < img.shape[0] and x < i+size:
+                y = j
+                while y < img.shape[1] and y < j+size:
+                    cnt = cnt+1
+                    mean = mean+img[x, y]
+                    y = y+1
+                x = x+1
+            mean = mean/(cnt)
+            x = i
+            while x < img.shape[0] and x < i+size:
+                y = j
+                while y < img.shape[1] and y < j+size:
+                    if img[x, y] <= mean-5:
+                        img[x, y] = 0
+                    else:
+                        img[x, y] = 255
+                    y = y+1
+                x = x+1
+            j = j+size
+        i = i+size
+    return img
+
+
 # Load an image in the greyscale
-img = cv2.imread('threshold.png', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('paper.png', cv2.IMREAD_GRAYSCALE)
+print(image.shape)
 # apply threshold finder
-vv1 = thres_finder(img, thres=30, delta_T=1.0)
+vv1 = thres_finder(image, thres=30, delta_T=1.0)
 # threshold the image
-thresh = global_threshold(img, vv1, 255, 0)
-ret, thresh_cv2 = cv2.threshold(img, vv1, 255, cv2.THRESH_BINARY)
+local_thresh = local_threshold(image, 7)
+thresh = global_threshold(image, vv1, 255, 0)
+#ret, thresh_cv2 = cv2.threshold(img, vv1, 255, cv2.THRESH_BINARY)
 # Display the image side by side
-out = cv2.hconcat([img, thresh, thresh_cv2])
+
+out = cv2.hconcat([image, thresh, local_thresh])
 cv2.imshow('threshold', out)
 cv2.waitKey(0)
